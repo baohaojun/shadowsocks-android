88cf67f314a05ac4f73613e0966d39b1c659a499
Bao Haojun <baohaojun@smartisan.cn>
2015-07-22 16:23:36 +0800

    remote-build: do the install too src/main/scala/com/github/shadowsocks/Shadowsocks.scala: fix it for lollipop
    Signed-off-by: Bao Haojun <baohaojun@smartisan.cn>

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-function try () {
-"$@" || exit -1
-}
-
-try pushd src/main
+set -e
+cd src/main
 
 # Build
-try $ANDROID_NDK_HOME/ndk-build clean
-try $ANDROID_NDK_HOME/ndk-build -j8
+ndk-build -j8
 
 # copy executables
 rm -rf assets/armeabi-v7a
@@ -17,8 +13,7 @@ mkdir -p assets/armeabi-v7a
 mkdir -p assets/x86
 for app in pdnsd redsocks ss-local ss-tunnel tun2socks
 do
-    try mv libs/armeabi-v7a/$app assets/armeabi-v7a/
-    try mv libs/x86/$app assets/x86/
+    mv libs/armeabi-v7a/$app assets/armeabi-v7a/
+    mv libs/x86/$app assets/x86/
 done
 
-try popd
